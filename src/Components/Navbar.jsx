95bb2443d303127ebf315d11927649c20859616e
Avinash <avinashshiyani760@gmail.com>
2024-10-22 23:43:37 +0530

    Added Navbar in this

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Logo from "../assets/Logo.png";
+
+const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center w-full py-[10px]">
+      <div className="flex items-center space-x-2">
+        <div>
+          <img src={Logo} alt="" />
+        </div>
+        <div className="">
+          <p className="text-[18px] font-bold">financy</p>
+        </div>
+      </div>
+      <div className="flex justify-center items-center">
+        <div className="h-[30px] w-[30px] rounded-full bg-green-300"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
