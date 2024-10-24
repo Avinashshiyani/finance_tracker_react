fa50a9fa5d26f6830a2a5810ed6d03bfcad21e1d
Avinash Shiyani <107192407+Avinashshiyani@users.noreply.github.com>
2024-10-24 12:09:40 +0530

    Operation Cards And responsiveness added (#3)
    * Added Dashboard Cards and small changes
    
    * Added Plus minus expence operation with other small changes

---
+++ src/Components/CardOperation.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { cardDetailsOne } from "./Info";
+import { twMerge } from "tailwind-merge";
+
+const CardOperation = ({ details = cardDetailsOne }) => {
+  return (
+    <div className="grid w-full  rounded-[12px] bg-white border-[1px]  border-gray-300">
+      <div className="flex items-center p-[20px]">
+        <div className="flex items-center justify-center">
+          <div
+            className={twMerge(
+              "h-[48px] w-[48px] flex justify-center items-center rounded-[8px]",
+              details.title == "Add expense" ? "bg-[#FEE4E2]" : "bg-[#DCFAE6]"
+            )}
+          >
+            <img src={details.image} alt="Plus sign" />
+          </div>
+          <div className="grid pl-[12px]">
+            <span className="text-[16px] font-semibold">{details.title}</span>
+            <span className="text-[14px] ">{details.info}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CardOperation;
--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,5 +1,21 @@
 import React from "react";
 import CardDashboard from "./CardDashboard";
+import CardOperation from "./CardOperation";
+import { cardDetailsTwo } from "./Info";
+
+const Dashboard = () => {
+  return (
+    <div className="grid items-center">
+      <div className="grid items-center gap-3 px-10 mt-[22px] sm:grid-cols-2 md:grid-cols-3">
+        <CardDashboard amt={19680} />
+        <CardDashboard title="Income" />
+        <CardDashboard title="Expences" />
+      </div>
+      <div className="grid  items-center gap-3 px-10 mt-[22px] sm:grid-cols-3">
+        <CardOperation />
+        <CardOperation details={cardDetailsTwo} />
+      </div>
+
 
 const Dashboard = () => {
   return (
