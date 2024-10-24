f1533ede56d502250dbf85c84424889b97ef1546
Avinash <avinashshiyani760@gmail.com>
2024-10-24 12:07:26 +0530

    Added Plus minus expence operation with other small changes

--- src/Components/Dashboard.jsx
+++ src/Components/Dashboard.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import CardDashboard from "./CardDashboard";
+import CardOperation from "./CardOperation";
+import { cardDetailsTwo } from "./Info";
 
 const Dashboard = () => {
   return (
-    <div className="grid grid-cols-3 items-center gap-3 px-10 mt-[22px]">
-      <CardDashboard amt={19680} />
-      <CardDashboard title="Income" />
-      <CardDashboard title="Expences" />
+    <div className="grid items-center">
+      <div className="grid items-center gap-3 px-10 mt-[22px] sm:grid-cols-2 md:grid-cols-3">
+        <CardDashboard amt={19680} />
+        <CardDashboard title="Income" />
+        <CardDashboard title="Expences" />
+      </div>
+      <div className="grid  items-center gap-3 px-10 mt-[22px] sm:grid-cols-3">
+        <CardOperation />
+        <CardOperation details={cardDetailsTwo} />
+      </div>
     </div>
   );
 };
---
+++ src/Components/Info.jsx
@@ -0,0 +1,16 @@
+import PlusSign from "../assets/plus.png";
+import MinusSign from "../assets/minus.png";
+
+let cardDetailsOne = {
+  title: "Add income",
+  info: "Create an income manually",
+  image: PlusSign,
+};
+
+let cardDetailsTwo = {
+  title: "Add expense",
+  info: "Create an expense manually",
+  image: MinusSign,
+};
+
+export { cardDetailsOne, cardDetailsTwo };
